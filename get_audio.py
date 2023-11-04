257eb33e8892a36311b5e83b1e076197eed34115
Devansh Kanda <101200047+Devanshkanda@users.noreply.github.com>
2023-11-04 18:35:24 +0530

    Add files via upload
    Updated with adding arguments like entering url , type of the file, name 
    added checks for invalid arguments and exception handlers

--- get_audio.py
+++ get_audio.py
@@ -1,16 +1,44 @@
 from pytube import YouTube
+import argparse
 
-def download_audio(url: str):
+def download_audio(url: str, type: str, filename: str):
     try:
         yt = YouTube(url=url)
-        audio_stream = yt.streams.filter(only_audio=True, mime_type="audio/mp4")
-        audio_stream.download()
-        print("audio downloaded successfully")
+        audio_stream = yt.streams.filter(only_audio=True, mime_type="audio/mp4").first()
+        # print(audio_stream)
+        if(audio_stream is None):
+            return "No file format is available"
+        
+        print("Downloading the file. please wait for few Seconds")
+
+        if not filename:
+            audio_stream.download()
+        else:
+            audio_stream.download(filename=f"{filename}.mp3")
+
+        print(f"{type} downloaded successfully")
     except Exception as e:
-        print("An error occured" + str(e))
+        print("An error occured : " + str(e))
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="""Welcome Here to convert your
+                                    favourate video to audio...""")
+    
+    parser.add_argument("--url", type=str, help="Enter the Url of the youtube video here")
+    parser.add_argument("--type", type=str, help="Enter the Type format of the file (audio/video)")
+    parser.add_argument("--name", type=str, help="Enter the name of file without .ext Example : 'audio' ", default="")
 
-    urli = input("Enter the URL of the Video : ")
-    download_audio(urli)
+    args = parser.parse_args()
+
+    url = args.url
+    filetype = args.type
+    name = args.name
+
+    if(args.url is None):
+            print("Enter the url .")
+            quit()
+    elif(filetype == "audio" or filetype == "video" and url):
+        download_audio(url, filetype, name)
+    else:
+        print("Please enter the correct input format")

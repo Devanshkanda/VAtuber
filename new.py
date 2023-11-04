d4f0b672e0132df990cd403fce5fee2ddf0180b8
Devansh Kanda <101200047+Devanshkanda@users.noreply.github.com>
2023-11-04 16:11:58 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,17 @@
+from pytube import YouTube
+
+def download_audio(url: str):
+    try:
+        yt = YouTube(url=url)
+        audio_stream = yt.streams.filter(only_audio=True, mime_type="audio/mp4")
+        # print(audio_stream)
+        audio_stream.download()
+        print("audio downloaded successfully")
+    except Exception as e:
+        print("An error occured" + str(e))
+
+
+if __name__ == "__main__":
+
+    urli = input("Enter the URL of the Video : ")
+    download_audio(urli)

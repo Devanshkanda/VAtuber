b3a1efca8d856d1791b7029638a04dac1ce016d9
Devansh Kanda <101200047+Devanshkanda@users.noreply.github.com>
2023-11-18 20:23:15 +0530

    Add files via upload
    added module to handle custom generated exceptions.

--- get_audio_video.py
+++ get_audio_video.py
@@ -2,31 +2,40 @@ from pytube import YouTube
 import argparse
 from pathlib import Path
 
+
+class customException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self) -> str:
+        return self.message
+
+
 class get_StreamFile:
 
     def download_file(self, url: str, type: str, filename: str, res: str) -> None:
         try:
             yt = YouTube(url=url)
             stream = None
-            extension = None
+            extension: str;
 
             if type == "audio":
                 stream = yt.streams.filter(only_audio=True, mime_type="audio/mp4", type="audio").first()
                 extension = "mp3"
 
                 if(stream is None):
-                    return "No file format Stream is available"
+                    raise customException("No file format Stream is available")
                 
             elif type == "video":
                 stream = yt.streams.filter(only_video=True, mime_type="video/mp4", type="video", res=res).first()
                 extension = "mp4"
 
                 if(stream is None):
-                    return "No file format Stream is available"
+                    raise customException("No file format Stream is available")
                 
             else:
-                print("Unknown file type entered ..")
-                quit()
+                raise customException("Unknown file type entered ..")
+                # quit()
 
             print("Downloading the file. please wait for few Seconds ...")
 
@@ -36,15 +45,18 @@ class get_StreamFile:
                 stream.download(filename=f"{filename}.{extension}")
 
             print(f"{type} downloaded successfully at Path: {Path.cwd()}")
+
+        except customException as e:
+            print(f"An error occured: {str(e)}")
         except Exception as e:
-            print("An error occured : " + str(e))
+            print(f"An error occured: {str(e)}")
 
 
     def create_arge_parser(self):
         parser = argparse.ArgumentParser(description="""Welcome Here to convert your
                                         favourate video to audio...""")
         
-        parser.add_argument("--url", type=str, help="Enter the Url of the youtube video here")
+        parser.add_argument("--url", type=str, help="Enter the Url of the youtube video here", default=None)
         parser.add_argument("--type", type=str, help="Enter the Type format of the file (audio/video)")
         parser.add_argument("--name", type=str, help="Enter the name of file without .ext Example : 'audio' ", default="")
         parser.add_argument("--res", type=str, help="Enter the resolution type when downloading video Example: '1080p'. Default set to 480p", default="480p")
@@ -53,17 +65,18 @@ class get_StreamFile:
 
 
 if __name__ == "__main__":
+
     obj = get_StreamFile()
     args = obj.create_arge_parser()
-    url = args.url
-    filetype = args.type
-    name = args.name
-    res = args.res
+    url: str = args.url
+    filetype: str = args.type
+    name: str = args.name
+    res: str = args.res
 
     if(url is None or not url):
         print("Enter the url .")
         quit()
     elif(filetype == "audio" and url) or (filetype == "video" and url):
-        obj.download_file(url, filetype, name, res)
+        obj.download_file(url=url, type=filetype, filename=name, res=res)
     else:
         print("Please enter the correct input format")

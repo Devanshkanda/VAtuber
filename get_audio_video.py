1383d3626b85842c3fd7e843f6e900e13b1f9618
Devansh Kanda <101200047+Devanshkanda@users.noreply.github.com>
2024-01-01 01:06:23 +0530

    Add files via upload
    Added Constructor implementation.

--- get_audio_video.py
+++ get_audio_video.py
@@ -15,21 +15,28 @@ class customException(Exception):
 
 class get_StreamFile:
 
-    def download_file(self, url: str, type: str, filename: str, res: str) -> None:
+    def __init__(self, url: list, type: str, filename: str, res: str) -> None:
+        self.urls: list = url
+        self.type: str = type
+        self.filename: str = filename
+        self.res: str = res
+
+
+    def download_file(self, url: str) -> None:
         try:
             yt = YouTube(url=url, on_progress_callback=on_progress)
             stream = None
             extension: str
 
-            if type == "audio":
+            if self.type == "audio":
                 stream = yt.streams.filter(only_audio=True, mime_type="audio/mp4", type="audio").first()
                 extension = "mp3"
 
                 if(stream is None):
                     raise customException("No file format Stream is available")
                 
-            elif type == "video":
-                stream = yt.streams.filter(only_video=True, mime_type="video/mp4", type="video", res=res).first()
+            elif self.type == "video":
+                stream = yt.streams.filter(only_video=True, mime_type="video/mp4", type="video", res=self.res).first()
                 extension = "mp4"
 
                 if(stream is None):
@@ -43,12 +50,12 @@ class get_StreamFile:
             print(f"Title: {stream.title}")
             print(f"filesize: {stream.filesize/(1024*1024)} MB")
 
-            if not filename:
+            if not self.filename:
                 stream.download()
             else:
-                stream.download(filename=f"{filename}.{extension}")
+                stream.download(filename=f"{self.filename}.{extension}")
 
-            print(f"{type} downloaded successfully at Path: {Path.cwd()}/{stream.title}")
+            print(f"{self.type} downloaded successfully at Path: {Path.cwd()}/{stream.title}")
 
         except customException as e:
             print(f"An error occured: {str(e)}")
@@ -56,22 +63,27 @@ class get_StreamFile:
             print(f"An error occured: {str(e)}")
 
 
-    def create_arge_parser(self):
+    @staticmethod
+    def create_arge_parser():
         parser = argparse.ArgumentParser(description="""Welcome Here to convert your
                                         favourate video to audio...""")
         
-        parser.add_argument("--url", nargs='*', help="""Enter the Url(s) of the youtube video here example : <url1> <url2> <url3> ...""", default=None)
+        parser.add_argument("--url", nargs='*', help="Enter the Url(s) of the youtube video here", default=None)
         parser.add_argument("--type", type=str, help="Enter the Type format of the file (audio/video)", default=None)
         parser.add_argument("--name", type=str, help="Enter the name of file without .ext Example : 'audio' ", default="")
         parser.add_argument("--res", type=str, help="Enter the resolution type when downloading video Example: '1080p'. Default set to 480p", default="480p")
 
         return parser.parse_args()
+    
+    
+    def start_download(self) -> None:
+        for i in self.urls:
+            threading.Thread(target=self.download_file, args=(i,)).start()
 
 
 if __name__ == "__main__":
 
-    obj = get_StreamFile()
-    args = obj.create_arge_parser()
+    args = get_StreamFile.create_arge_parser()
     url: list = args.url
     filetype: str = args.type
     name: str = args.name
@@ -81,7 +93,7 @@ if __name__ == "__main__":
         print("Enter the url .")
         quit()
     elif(filetype == "audio" and url) or (filetype == "video" and url):
-        for i in url:
-            threading.Thread(target=obj.download_file, args=(i, filetype, name, res,)).start()
+        get_streamFile_obj = get_StreamFile(url=url, type=filetype, filename=name, res=res)
+        get_streamFile_obj.start_download()
     else:
         print("Please enter the correct input format")
